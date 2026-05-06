416f397c0d8c41613c24f6104d8d34c6310182ea
Seu Nome <seu-email@exemplo.com>
2026-05-06 13:58:58 -0300

    Definição de basename no BrowserRouter para compatibilidade com GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/connector-insights">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/calculo-mensal" element={<CalculoMensal />} />
